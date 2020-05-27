799ecc6fc8d0673fbd21e620af2bd6c54356c77e
tatianamac <tatiana.t.mac@gmail.com>
2020-05-27 10:52:17 -0700

    ⚒️ Converts to TypeScript

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,7 +4,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Tatiana'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Shitpost Archive'
 
 export default function Layout({ children, home }) {
   return (
@@ -13,7 +13,7 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="An archive of all of my shitposts, for better or worse"
         />
         <meta
           property="og:image"
---
+++ pages/_app.tsx
@@ -0,0 +1,7 @@
+import '../styles/global.css'
+
+import { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
+	return <Component {...pageProps} />
+}
